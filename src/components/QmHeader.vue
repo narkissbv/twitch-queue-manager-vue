ad04fcdec8e2b19e606bf9f204d3bd76454b6695
narkissbv <matan@matannarkiss.com>
2022-05-24 09:20:06 +0300

    add refresh

--- src/components/QmHeader.vue
+++ src/components/QmHeader.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="title">Queue Manager</div>
+  <div class="action refresh" @click="refresh"></div>
 </template>
 
 <script setup>
+  import { useStore } from 'vuex'
+
+  const store = useStore()
+  const refresh = async () => {
+    store.dispatch('setLoader', true);
+    await store.dispatch('fetchList')
+    store.dispatch('setLoader', false);
+  }
 
 </script>
 
@@ -13,5 +22,18 @@
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     font-size: 30px;
+  }
+
+  .action {
+    position: absolute;
+    top: 10px;
+    width: 24px;
+    height: 24px;
+    background-size: contain;
+    cursor: pointer;
+  }
+  .action.refresh {
+    left: 10px;
+    background-image: url('~@/assets/refresh.svg');
   }
 </style>
